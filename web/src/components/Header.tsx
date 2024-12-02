345a11364a0a853890f445859b0eec866ba50228
Lucas Costa <francisco.lucas0507@gmail.com>
2024-12-02 15:49:13 -0300

    web: basic editor layout

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
-    <div className="flex items-center justify-between bg-gray-100 h-16 px-2 border-b-violet-400 border-b-2 border-opacity-60">
-      <span className="font-bold text-2xl">ALC-LISP</span>
+    <div className="flex items-center justify-between bg-gray-100 h-16 px-2">
+      <span className="font-bold text-2xl text-violet-400 ">ALC-LISP</span>
     </div>
   );
 }
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,16 +1,52 @@
+import { Editor } from "@monaco-editor/react";
 import { Counter } from "../components/counter";
 import { Header } from "../components/Header";
 import { App } from "./App";
 
 export function Home() {
-  console.log("Hello");
-
   return (
-    <div>
+    <div className="flex flex-1 h-full flex-col">
       <Header />
       <App>
-        <h1>Home</h1>
-        <Counter />
+        <div className="flex flex-1 flex-grow flex-row justify-stretch w-full h-full">
+          <div className="flex flex-col flex-1 border-r-violet-400 border-r-2 border-opacity-60">
+            <div className="flex flex-row border-y-violet-400 border-y-2 border-opacity-60">
+              <button className="bg-violet-400 active:bg-violet-300  text-white px-4 py-1 outline-none">
+                Run
+              </button>
+            </div>
+            <Editor
+              className="flex flex-1 h-full"
+              defaultLanguage="clojure"
+              theme="vs-light"
+              height="100%"
+              width="100%"
+              options={{
+                lineNumbers: "on",
+                language: "lisp",
+                bracketPairColorization: {
+                  enabled: true,
+                  independentColorPoolPerBracketType: true,
+                },
+              }}
+              defaultValue={`; Hello World program in alc-lisp\n\n(print "Hello World")`}
+            />
+          </div>
+          <div className="flex flex-col flex-1">
+            <div className="flex flex-row border-y-violet-400 border-y-2 border-opacity-60">
+              <button className="bg-violet-400 active:bg-violet-300  text-white px-4 py-1 outline-none">
+                Output
+              </button>
+              <button className="bg-violet-400 active:bg-violet-300  text-white px-4 py-1 outline-none">
+                Tokens
+              </button>
+              <button className="bg-violet-400 active:bg-violet-300  text-white px-4 py-1 outline-none">
+                AST
+              </button>
+            </div>
+            <Counter />
+          </div>
+        </div>
       </App>
     </div>
   );
