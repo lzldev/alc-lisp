3e40dcc0e383b60aba126a5c127648df23b73703
Lucas Costa <francisco.lucas0507@gmail.com>
2024-12-03 16:23:20 -0300

    wasm: wasm builtins
    web: run functionality

--- alc-lisp-wasm/src/lib.rs
+++ alc-lisp-wasm/src/lib.rs
@@ -1,4 +1,13 @@
+use std::{cell::LazyCell, collections::HashMap, panic, rc::Rc};
+
+use alc_lisp::{
+    ast::AST,
+    interpreter::{builtins::add_generic_builtins, objects::Object, Env, Program, NULL},
+    lexer::Lexer,
+};
+use log::info;
 use wasm_bindgen::prelude::*;
+use web_sys::{Performance, Window};
 
 #[wasm_bindgen]
 pub fn add(left: u64, right: u64) -> u64 {
@@ -8,6 +17,74 @@ pub fn add(left: u64, right: u64) -> u64 {
 #[wasm_bindgen]
 pub fn add2(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[wasm_bindgen(start)]
+pub fn init() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init().expect("console_log::init");
+}
+
+pub const WINDOW: LazyCell<Window> = LazyCell::new(|| web_sys::window().expect("window not found"));
+pub const PERFORMANCE: LazyCell<Performance> =
+    LazyCell::new(|| WINDOW.performance().expect("performance not found"));
+
+pub fn add_wasm_builtins(env: &mut Env) {
+    env.insert(
+        "print".into(),
+        Rc::new(Object::Builtin(|args| {
+            info!(
+                "{}",
+                args.iter().map(|v| format!("{}", v)).collect::<String>()
+            );
+
+            return NULL.clone();
+        })),
+    );
+
+    env.insert(
+        "debug".into(),
+        Rc::new(Object::Builtin(|args| {
+            info!(
+                "{:?}",
+                args.iter().map(|v| format!("{}", v)).collect::<String>()
+            );
+
+            return NULL.clone();
+        })),
+    );
+}
+
+#[wasm_bindgen]
+pub fn run(code: String) {
+    let mut lexer = Lexer::from_string(code);
+
+    lexer.parse().expect("lexer::parse");
+
+    let tokens = lexer.tokens();
+
+    let mut ast = AST::with_tokens(tokens);
+
+    let root = ast.parse().expect("ast::parse");
+
+    if ast.has_errors() {
+        ast.print_errors(&root);
+        panic!("ast::has_errors");
+    }
+
+    let mut globals: Env = HashMap::new();
+    add_generic_builtins(&mut globals);
+    add_wasm_builtins(&mut globals);
+
+    let mut program = Program::new(globals);
+
+    let start = PERFORMANCE.now(); // TODO:Remove Timing code
+    let result = program.eval(&root).expect("program::eval");
+    let end = PERFORMANCE.now() - start; // TODO:Remove Timing code
+
+    info!("took:{:.4}ms", end);
+
+    info!("result:{}", result);
 }
 
 #[cfg(test)]
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,8 +2,12 @@ import { Editor } from "@monaco-editor/react";
 import { Counter } from "../components/counter";
 import { Header } from "../components/Header";
 import { App } from "./App";
+import { useRef } from "react";
+import { run } from "alc-lisp-wasm";
 
 export function Home() {
+  const editorRef = useRef<any>(null);
+
   return (
     <div className="flex flex-1 h-full flex-col">
       <Header />
@@ -11,11 +15,22 @@ export function Home() {
         <div className="flex flex-1 flex-grow flex-row justify-stretch w-full h-full">
           <div className="flex flex-col flex-1 border-r-violet-400 border-r-2 border-opacity-60">
             <div className="flex flex-row border-y-violet-400 border-y-2 border-opacity-60">
-              <button className="bg-violet-400 active:bg-violet-300  text-white px-4 py-1 outline-none">
+              <button
+                className="bg-violet-400 active:bg-violet-300  text-white px-4 py-1 outline-none"
+                onClick={() => {
+                  const code = editorRef.current?.getValue();
+
+                  console.log("code", code);
+                  run(code);
+                }}
+              >
                 Run
               </button>
             </div>
             <Editor
+              onMount={(editor, monaco) => {
+                editorRef.current = editor;
+              }}
               className="flex flex-1 h-full"
               defaultLanguage="clojure"
               theme="vs-light"
