53316f158ebfd14d3393bf2364a472fa7fe64339
Lucas Costa <francisco.lucas0507@gmail.com>
2024-12-12 05:49:30 -0300

    feat: object into wasm_bindgen js object

--- src/interpreter/objects/mod.rs
+++ src/interpreter/objects/mod.rs
@@ -3,6 +3,9 @@ use std::{
     fmt::Display,
 };
 
+use js_sys::JsString;
+use wasm_bindgen::JsValue;
+
 use crate::ast::Node;
 
 use super::{Env, Reference, NULL};
@@ -84,6 +87,42 @@ impl Display for Object {
             Object::Error(msg) => {
                 write!(f, "Error:{}", msg)
             }
+        }
+    }
+}
+
+#[cfg(feature = "wasm")]
+pub const BUILTIN_MESSAGE: LazyCell<wasm_bindgen::JsValue> =
+    LazyCell::new(|| JsString::from("Builtin Function").into());
+
+#[cfg(feature = "wasm")]
+impl From<Object> for wasm_bindgen::JsValue {
+    fn from(value: Object) -> Self {
+        match value {
+            Object::Null => wasm_bindgen::JsValue::NULL,
+            Object::Integer(value) => wasm_bindgen::JsValue::from(value as i32),
+            Object::String(st) => JsString::from(st).into(),
+            Object::Bool(value) => {
+                if value {
+                    wasm_bindgen::JsValue::TRUE
+                } else {
+                    wasm_bindgen::JsValue::FALSE
+                }
+            }
+            Object::List(vec) => {
+                let array = js_sys::Array::new_with_length(vec.len() as u32);
+
+                vec.into_iter().enumerate().for_each(|(idx, arg)| {
+                    array.set(idx as u32, JsValue::from((*arg).clone()));
+                });
+
+                array.into()
+            }
+            Object::Builtin { .. } => BUILTIN_MESSAGE.clone(),
+            Object::Function { env, .. } => {
+                JsString::from(format!("FUNCTION [{:p}]", env.as_ptr())).into()
+            }
+            Object::Error(_) => todo!(),
         }
     }
 }
