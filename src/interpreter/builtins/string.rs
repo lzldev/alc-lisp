41eda1231e6cae6ab52143bdb650ae51a61db0b4
Lucas Costa <francisco.lucas0507@gmail.com>
2024-12-18 05:13:57 +0000

    docs: builtin strings module

--- src/interpreter/builtins/string.rs
+++ src/interpreter/builtins/string.rs
@@ -1,3 +1,4 @@
+//! Builtin functions for string operations
 use std::rc::Rc;
 
 use crate::interpreter::{objects::Object, Env, Reference, STRING};
@@ -5,65 +6,60 @@ use crate::interpreter::{objects::Object, Env, Reference, STRING};
 use super::{errors::new_args_len_error, typecheck_args};
 
 pub fn add_string_builtins(env: &mut Env) {
-    env.insert(
-        "str".into(),
-        Rc::new(Object::Builtin {
-            function: |args| {
-                if let Some(err) = typecheck_args(
-                    "str",
-                    STRING.type_of(),
-                    |obj| !matches!(obj.as_ref(), Object::String(_)),
-                    &args,
-                ) {
-                    return err;
-                }
+    env.insert("str".into(), Rc::new(Object::Builtin { function: str }));
+    env.insert("lines".into(), Rc::new(Object::Builtin { function: lines }));
+}
 
-                let result = args
-                    .iter()
-                    .map(|v| {
-                        let Object::String(inner) = v.as_ref() else {
-                            panic!("This should never happen");
-                        };
+/// Concatenates the arguments into a string
+pub fn str(args: Vec<Reference>) -> Reference {
+    if let Some(err) = typecheck_args(
+        "str",
+        STRING.type_of(),
+        |obj| !matches!(obj.as_ref(), Object::String(_)),
+        &args,
+    ) {
+        return err;
+    }
 
-                        inner
-                    })
-                    .cloned()
-                    .collect::<String>();
+    let result = args
+        .iter()
+        .map(|v| {
+            let Object::String(inner) = v.as_ref() else {
+                panic!("This should never happen");
+            };
 
-                Rc::new(Object::String(result))
-            },
-        }),
-    );
+            inner
+        })
+        .cloned()
+        .collect::<String>();
 
-    env.insert(
-        "lines".into(),
-        Rc::new(Object::Builtin {
-            function: |args| {
-                let len = args.len();
-                if len != 1 {
-                    return new_args_len_error("lines", &args, 1);
-                }
+    Rc::new(Object::String(result))
+}
 
-                if let Some(err) = typecheck_args(
-                    "lines",
-                    STRING.type_of(),
-                    |obj| !matches!(obj.as_ref(), Object::String(_)),
-                    &args,
-                ) {
-                    return err;
-                }
+/// Splits a string into a list of lines
+fn lines(args: Vec<Reference>) -> Reference {
+    let len = args.len();
+    if len != 1 {
+        return new_args_len_error("lines", &args, 1);
+    }
 
-                let Object::String(inner) = args[0].as_ref() else {
-                    panic!("This should never happen");
-                };
+    if let Some(err) = typecheck_args(
+        "lines",
+        STRING.type_of(),
+        |obj| !matches!(obj.as_ref(), Object::String(_)),
+        &args,
+    ) {
+        return err;
+    }
 
-                let lines = inner
-                    .split("\n")
-                    .map(|v| Reference::new(Object::String(v.to_owned())))
-                    .collect::<Vec<_>>();
+    let Object::String(inner) = args[0].as_ref() else {
+        panic!("This should never happen");
+    };
 
-                Rc::new(Object::List(lines))
-            },
-        }),
-    );
+    let lines = inner
+        .split("\n")
+        .map(|v| Reference::new(Object::String(v.to_owned())))
+        .collect::<Vec<_>>();
+
+    Rc::new(Object::List(lines))
 }
